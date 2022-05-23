ea538b710d0184774e34f880e5dbeadd68c924c8
Pixis <pixis@Pixiss-MacBook-Pro-2.local>
2022-05-23 21:49:38 +0530

    Graph Misc

---
+++ graphs/shortestPath.js
@@ -0,0 +1,35 @@
+const shortestPath = (edges, nodeA, nodeB) => {
+    const graph = buildGraph(edges)
+    const queue = [[nodeA, 0]]
+    const visited = new Set([nodeA])
+
+    while(queue.length > 0) {
+        const [current, distance]  = queue.shift();
+
+        if(current === nodeB) return distance
+
+        for (let neighbor of graph[current]) {
+            if(!visited.has(neighbor)) {
+                visited.add(neighbor);
+                queue.push([neighbor, visited + 1])
+            }
+        }
+    }
+
+    return -1
+}
+
+const buildGraph = (edges) => {
+    const graph = {};
+
+    for(let edge of edges) {
+        const [a, b] = edge
+        if (!(a in graph)) graph[a] = []
+        if (!(b in graph)) graph[b] = []
+
+        graph[a].push(b)
+        graph[b].push(a)
+    }
+
+    return graph;
+}
